54225d7cca672bfe118cd77cc85cd58687714a8a
saaim <125155759+saaim12@users.noreply.github.com>
2025-02-28 22:46:13 +0500

    bubble sort completed

--- src/ARRAYS/Arrays_Sortings.java
+++ src/ARRAYS/Arrays_Sortings.java
@@ -16,12 +16,12 @@ public class Arrays_Sortings {
             return arr;
         }
         for (int i = 0; i < arr.length ; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if(arr[i]>arr[j]){
+            for (int j = 1; j <=arr.length-i-1; j++) {
+                if(arr[j-1]>arr[j]){
                     //this is XOR swapping technique which dont requires and additional variable
-                    arr[i]=arr[i]^arr[j];
-                    arr[j]=arr[i]^arr[j];
-                    arr[i]=arr[i] ^ arr[j];
+                    arr[j]=arr[j]^arr[j-1];
+                    arr[j-1]=arr[j]^arr[j-1];
+                    arr[j]=arr[j] ^ arr[j-1];
                 }
             }
         }
