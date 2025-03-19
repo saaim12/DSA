a0e519385ba4de83e433c0c6e252071fc9378e93
saaim <125155759+saaim12@users.noreply.github.com>
2025-03-20 01:31:57 +0500

    resursions starting second video hald till 32:18

--- src/Recursions/Recursions.java
+++ src/Recursions/Recursions.java
@@ -2,25 +2,41 @@ package Recursions;
 
 public class Recursions {
     public static void main(String[] args) {
-       // printing(10);
-        fibbonachi_sequence(53);
+        //printing problem
+    //printing(0,33);
+        //factoiral problem
+      //  System.out.println(factorial(8));
+        //sum probelm
+        //like sum of 1342=10 ,  1111=4
+        System.out.println(sum(-123));
+        System.out.println(product(111223344));
     }
     //simple printing function
-    public static void printing(int num){
-        if(num==0){
+    public static void printing(int num,int limit){
+        if(num>=limit){
             System.out.println(num);
             return;
         }
         System.out.println(num);
-        printing(num-1);
+        printing(num+1,limit);
     }
-    //fibbonachi sequence
-    public static int fibbonachi_sequence(int num){
-        if(num<2){
-            System.out.println(num);
-            return 0;
+    public static int factorial(int num){
+        if(num==0){
+            return 1;
         }
-        System.out.println(num);
-        return fibbonachi_sequence(num-1) + fibbonachi_sequence(num-2);
+        return  num * factorial(num-1);
     }
+    public static int sum(int num){
+        if(num==0){
+            return num;
+        }
+        return (num%10) + sum(num/10);
+    }
+    public static int product(int num){
+        if(num==0){
+            return 1;
+        }
+        return (num%10) * product(num/10);
+    }
+
 }
