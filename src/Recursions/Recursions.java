18ec47c9470590d601f65f93aaa8901092574126
saaim <125155759+saaim12@users.noreply.github.com>
2025-03-20 06:45:21 +0500

    reversing a number

--- src/Recursions/Recursions.java
+++ src/Recursions/Recursions.java
@@ -8,8 +8,10 @@ public class Recursions {
       //  System.out.println(factorial(8));
         //sum probelm
         //like sum of 1342=10 ,  1111=4
-        System.out.println(sum(-123));
-        System.out.println(product(111223344));
+//        System.out.println(sum(-123));
+//        System.out.println(product(111223344));
+        //reversing a number
+       // System.out.println(reverse_num(1234));
     }
     //simple printing function
     public static void printing(int num,int limit){
@@ -26,17 +28,28 @@ public class Recursions {
         }
         return  num * factorial(num-1);
     }
+    //sum through recursion
     public static int sum(int num){
         if(num==0){
             return num;
         }
         return (num%10) + sum(num/10);
     }
+    //product through recursion
     public static int product(int num){
         if(num==0){
             return 1;
         }
         return (num%10) * product(num/10);
     }
+    //reverse a number through recursion
+//    public static int reverse_num(int num){
+//        if(num==0){
+//            return 0;
+//        }
+//        int digit=num%10;
+//        return (digit*10) + reverse_num(num/10);
+//    }
+
 
 }
