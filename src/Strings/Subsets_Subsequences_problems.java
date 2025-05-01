de02aa5bc2bdbeef612fe2473fefb84cc893aed3
saaim <125155759+saaim12@users.noreply.github.com>
2025-05-01 15:43:34 +0500

    string problem solved

--- src/Strings/Subsets_Subsequences_problems.java
+++ src/Strings/Subsets_Subsequences_problems.java
@@ -1,15 +1,17 @@
 package Strings;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Subsets_Subsequences_problems {
     public static void main(String[] args) {
     String str="abc";
-       // System.out.println(subseq("",str,new ArrayList<String>()));
+       System.out.println(subseq("",str,new ArrayList<String>()));
         //to print a ascii number of a char
         System.out.println('a'+0);
         //type casting it
         System.out.println((char) ('a'+2));
+        System.out.println(subseq_iterative_method("123"));
     }
     // First subsequences problem
     public static ArrayList<String> subseq(String processed, String unprocessed, ArrayList<String> list) {
@@ -32,5 +34,21 @@ public class Subsets_Subsequences_problems {
 
         return list;
     }
+    public static List<List<Character>> subseq_iterative_method(String unprocessed) {
+        char[] arr = unprocessed.toCharArray(); // Convert string to char array
+        List<List<Character>> outer = new ArrayList<>(); // This will store all subsequences
+        outer.add(new ArrayList<>()); // Add an empty list representing the empty subsequence
 
+        for (char ch : arr) {
+            int n = outer.size(); // Get the current size of the outer list
+
+            for (int i = 0; i < n; i++) {
+                List<Character> internal = new ArrayList<>(outer.get(i)); // Copy existing subsequence
+                internal.add(ch); // Add the current character to the new subsequence
+                outer.add(internal); // Add the new subsequence to the list
+            }
+        }
+
+        return outer;
+    }
 }
